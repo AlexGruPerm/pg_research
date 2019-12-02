d9b2471929fccdbd525fff2b2ecbde6b00665e6e
AlexGruPerm <4872931alex#>
2019-12-03 01:37:44 +0500

    control home

--- src/main/scala/application/PgResearch.scala
+++ src/main/scala/application/PgResearch.scala
@@ -72,7 +72,7 @@ object PgResearch extends App {
     if (argsList.length == 0)
       //todo: don't forget replace succeed with fail.
       //Task.fail(new Exception("No input test config file, use: scala PgResearch <filename.json>"))
-      Task.succeed("C:\\pg_research\\src\\main\\resources\\loadconf.json")
+      Task.succeed("/home/gdev/data/home/data/PROJECTS/pg_research/src/main/resources/loadconf.json"/*"C:\\pg_research\\src\\main\\resources\\loadconf.json"*/)
     else
       Task.succeed(argsList(0))
 
